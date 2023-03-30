3e998f13cea7941525c93d64c6265b7fae14cd11
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-03-30 19:39:18 +0600

    fetch data done

--- src/Components/SearchField/Search.jsx
+++ src/Components/SearchField/Search.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Search = () => {
+
+    const [searchValue,setValue] = useState('');
+    const [datas,setDatas] = useState([])
+
+    function searchField(event){
+        setValue(event.target.value)
+    }
+    // console.log(searchValue)
+
+    useEffect(()=>{
+        fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${searchValue}`)
+        .then(res => res.json())
+        .then(datas => setDatas(datas))
+    },[searchValue,datas])
+
+    function getData(){
+        console.log(datas)
+    }
     return (
         <div className='flex gap-4 justify-center mt-7'>
-           <input type="text" placeholder="Type here" className="input input-bordered input-warning w-full max-w-xs" />
-           <button className="btn btn-warning">Warning</button>
+           <input onChange={searchField} type="text" placeholder="Type here" className="input input-bordered input-warning w-full max-w-xs" />
+           <button onClick={getData} className="btn btn-warning">Warning</button>
         </div>
     );
 };
